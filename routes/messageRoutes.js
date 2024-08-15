69cf5c7542ef40e57962c5c678dbfe2b79a45bcb
João Oliveira <jaoovit@gmail.com>
2024-08-15 14:24:09 +0100

    create a form to input messages

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,6 +3,6 @@ const messageController = require("../controllers/messageController");
 
 const router = express.Router();
 
-router.get("/", messageController.getMessages);
+router.get("/new", messageController.getMessages);
 
 module.exports = router;
