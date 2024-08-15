2f1c97a6f26b14215280f13992216f06b8ca98c3
João Oliveira <jaoovit@gmail.com>
2024-08-15 14:17:29 +0100

    use the ejs to show 2 messages to the message board

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 
 const express = require("express");
+const messageRouter = require("./routes/messageRoutes");
 const path = require("node:path");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use("/messages", messageRouter);
 
 // set the port with the dotenv
 const PORT = process.env.PORT;
@@ -12,9 +21,32 @@ const PORT = process.env.PORT;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// define the style route
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 app.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+});
+
+/*
+app.get("/new", (req, res) => {
   res.render("index");
 });
+*/
 
 app.listen(PORT, () => {
   console.log(`This server is running in ${PORT} port`);
---
+++ controllers/messageController.js
@@ -0,0 +1,5 @@
+const asyncHandler = require("express-async-handler");
+
+const getMessages = asyncHandler(async (req, res) => {});
+
+module.exports = { getMessages };
